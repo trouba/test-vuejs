730a2c0263c7a118681005db88cc45e92d63e160
trouba <trouba@gmail.com>
2018-11-24 14:09:05 +0100

    add header

---
+++ src/components/Header.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="header">
+    <div class="logo">logo</div>
+    <div class="basket-button">
+      <div class="button"><i class="fas fa-shopping-cart"></i> Cart (<span>0</span>) </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+.header {
+  display: flex;
+  justify-content: space-between;
+  background-color: #f84a1e;
+  color:#fff;
+  padding:20px;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <img alt="Park illustration" src="../assets/park.svg">
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
